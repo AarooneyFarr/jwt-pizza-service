c6a8826231cb1696559474cbb5f917b4cb1fe70c
Aaron Farr <theaaronfarr@gmail.com>
2024-10-01 10:15:23 -0600

    #d3 fixed tests

--- src/routes/orderRouter.test.js
+++ src/routes/orderRouter.test.js
@@ -1,10 +1,27 @@
 const request = require('supertest')
 const app = require('../service')
+const { Role, DB } = require('../database/database.js')
 
 let adminUserAuthToken
 
+function randomName() {
+	return Math.random().toString(36).substring(2, 12)
+}
+
+async function createAdminUser() {
+	let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] }
+	user.name = randomName()
+	user.email = user.name + '@admin.com'
+
+	await DB.addUser(user)
+
+	user.password = 'toomanysecrets'
+	return user
+}
+
 beforeAll(async () => {
-	const registerRes = await request(app).put('/api/auth').send({ email: 'a@jwt.com', password: 'admin' })
+	const ad = await createAdminUser()
+	const registerRes = await request(app).put('/api/auth').send({ email: ad.email, password: ad.password })
 	adminUserAuthToken = registerRes.body.token
 
 	if (process.env.VSCODE_INSPECTOR_OPTIONS) {
