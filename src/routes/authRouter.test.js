c8f74e8cfa06328fb76bbc5015c7befd73898b94
Aaron Farr <theaaronfarr@gmail.com>
2024-10-01 10:27:43 -0600

    #d3 added auth test

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -4,11 +4,13 @@ const app = require('../service')
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' }
 let testUserAuthToken
+let userID
 
 beforeAll(async () => {
 	testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com'
 	const registerRes = await request(app).post('/api/auth').send(testUser)
 	testUserAuthToken = registerRes.body.token
+	userID = registerRes.body.user.id
 })
 
 describe('Auth functions', () => {
@@ -19,6 +21,13 @@ describe('Auth functions', () => {
 
 		const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] }
 		expect(loginRes.body.user).toMatchObject(user)
+	})
+	test('update user', async () => {
+		const loginRes = await request(app)
+			.put(`/api/auth/${userID}`)
+			.send({ email: Math.random().toString(36).substring(2, 12) + '@test.com', password: 'borkyneu' })
+			.set('Authorization', `Bearer ${testUserAuthToken}`)
+		expect(loginRes.status).toBe(200)
 	})
 	// test('logout', async () => {
 	// 	const loginRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`)
